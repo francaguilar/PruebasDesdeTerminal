a93cad60cb2095f43a98221b7b2fed7e88787aa7
francaguilar <francisco.aguilar.carton@gmail.com>
2016-04-16 22:13:09 +0200

    cambios varios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Pruebas desde el Terminal de Debian
+
+##He creado una nueva rama, a ver que sucede
--- holaMundo.c
+++ holaMundo.c
@@ -1,6 +1,24 @@
-include <stdio.h>
+#include <stdio.h>
+#define TAM_MAXIMO 80
 
 int main() {
+	
+	char cadena [TAM_MAXIMO];
+	int entero1, entero2;
+	float decimal;
 	printf("hello world");
-return 0;
+	printf("Por favor, escribe una linea de no mas de 80 caracteres:\n");
+	gets(cadena);
+	printf("La linea introducida es:\n");
+	puts(cadena);
+	printf("Introduce dos enteros separados por un espacio: \n");
+	scanf("%d %d", &entero1, &entero2);
+	printf("Introduce un numero decimal:\n");
+	scanf("%f", &decimal);
+	printf("Introduce una cadena:\n");
+	scanf("%s", cadena);
+	printf("Has escrito lo siguiente:\n");
+	printf("%d %d %.2f %s", entero1, entero2, decimal, cadena);
+	
+	return 0;
 }
